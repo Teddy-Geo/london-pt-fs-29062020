9babddcd529a98beada2064eb91e65ffacb80b27
Teddy Geo <reftea@gmail.com>
2020-07-01 20:27:40 +0100

    added index to 01-what-is-js

---
+++ module-1/01-js-what-is-js/index.js
@@ -0,0 +1,9 @@
+let counter = 0;
+let increment = 5;
+
+counter = counter + increment;
+console.log(counter + ' 1');
+counter = counter + increment;
+console.log(counter + ' 2');
+counter = counter + increment;
+console.log(counter + ' 3');
