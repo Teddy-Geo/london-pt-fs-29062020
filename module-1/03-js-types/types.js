a8ab3770d3acdd6b10a2fd85cbb7367776a84f58
Teddy Geo <reftea@gmail.com>
2020-07-03 16:30:22 +0100

    added types.js to 03-js-types

---
+++ module-1/03-js-types/types.js
@@ -0,0 +1,22 @@
+const firstName = "Teo";
+
+console.log(typeof firstName, firstName);
+
+let age = 5;
+console.log(typeof age, age);
+
+let likesChicken = false;
+console.log(typeof likesChicken, likesChicken);
+
+let drivingLicense = undefined;
+console.log(typeof drivingLicense, drivingLicense);
+
+const surname = "Geo";
+console.log(typeof surname, surname);
+console.log("***********");
+
+console.log("Name: ", firstName);
+console.log("Age: ", age);
+console.log("Likes Chicken: ", likesChicken);
+console.log("Driving License: ", drivingLicense);
+console.log("Surname: ", surname);
