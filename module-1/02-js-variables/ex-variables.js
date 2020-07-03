bccb1ae1eec8e5ffc94dcae14116353cba518f08
Teddy Geo <reftea@gmail.com>
2020-07-03 08:48:26 +0100

    added extra variable exercise/ ex-variable.js

---
+++ module-1/02-js-variables/ex-variables.js
@@ -0,0 +1,18 @@
+const name = "Sam";
+let age = 29;
+
+console.log("Name :", name);
+console.log("Age: ", age);
+
+age = 30;
+console.log("New Age: ", age);
+ 
+age = "Thirty one";
+console.log("New Age in words: ", age);
+
+//name = "Joe";
+
+//console.log("New name: ", name); error due to name is assigned to const and can't be reassigned
+
+//const name;
+// will produce error missing initializer in const declaration because now is undefined and it always going to be undefined "name ==undefined"
