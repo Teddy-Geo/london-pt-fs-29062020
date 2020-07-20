8ec6596a3a694826661bccf20b9506084da515e1
Teddy Geo <reftea@gmail.com>
2020-07-20 17:09:35 +0100

    excercise 13 completed and pass the test

--- module-1/13-creating-arrays-exercise/index.js
+++ module-1/13-creating-arrays-exercise/index.js
@@ -4,7 +4,7 @@
  * Create a variable "wildAnimals" and and 'tiger', 'rabbit',
  * 'panda' and 'giraffe' into it as strings
  */
-
+let wildAnimals = ['tiger', 'rabbit', 'panda', 'giraffe'];
 
 /**
  * Exercise 2
@@ -12,7 +12,7 @@
  * Create a variable "pets" and and 'cat', 'dog' and 'hamster'
  * into it as strings(must contains dog)
  */
-
+let pets = ['cat', 'dog', 'hamster'];
 
 /**
  * Exercise 3
@@ -20,7 +20,7 @@
  * Create a variable "animals". It should include all wildAnimals
  * and all pets. Find the way to concatenate your arrays with JS
  */
-
+let animals = wildAnimals.concat(pets);
 
 /**
  * Exercise 4
@@ -29,6 +29,7 @@
  * find dog index in "animals" array
  */
 
+ let dogIndex = animals.indexOf('dog');
 
 /**
  * Exercise 5
@@ -36,7 +37,7 @@
  * create a pandaIndex variable and by using "indexOf" JS method
  * find dog index in "animals" array
  */
-
+let pandaIndex = animals.indexOf('panda');
 
 /**
  * create a variable with name "numbers"  which will be an array
@@ -44,6 +45,7 @@
  * (each number should be twice in this array).
  */
 
+let numbers = [1,2,3,4,5,6,7,8,9,9,8,7,6,5,4,3,2,1];
 
 /**
  * Exercise 6
@@ -52,6 +54,7 @@
  * find index of first instance of digit 4
  */
 
+let first4DigitIndex = numbers.indexOf(4);
 
 /**
  * Exercise 7
@@ -59,7 +62,7 @@
  * Create variable "last4DigitIndex" and from "numbers"
  * find index of last instance of digit 4
  */
-
+let last4DigitIndex = numbers.lastIndexOf(4);
 
 /**
  * Exercise 8
@@ -67,7 +70,8 @@
  * Create variable "hasSeven" check does "numbers"
  * contains 7
  */
-
+let hasSeven = numbers.includes(7);
+//console.log(hasSeven);
 
 /**
  * Exercise 9
@@ -76,6 +80,7 @@
  * contains 20
  */
 
+let hasTwenty = numbers.includes(20);
 
 /**
  * Exercise 10
@@ -84,6 +89,9 @@
  * "numbers" that are more than 4
  */
 
+let moreThanFour = numbers.find(function(num){
+  return num > 4;
+});
 
 /**
  * Exercise 11
@@ -91,4 +99,8 @@
  * Create variable "moreThanSevenIndex" returns first number
  * "numbers" that are more than 7
  */
+
+ let moreThanSevenIndex = numbers.find(function(num){
+   return num >= 7;
+ });
 
