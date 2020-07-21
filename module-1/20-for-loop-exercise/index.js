dad9cf419b5b1de1b3d81f41982253b34b290757
Teddy Geo <reftea@gmail.com>
2020-07-21 14:02:36 +0100

    exercise 20 completed

--- module-1/20-for-loop-exercise/index.js
+++ module-1/20-for-loop-exercise/index.js
@@ -10,6 +10,15 @@ const visitors = ["Alex", "Mike", "Sam"]; // Add at least 5 names
  * console.log "Hi {name}, my name is {myName}!"
  */
 
+ function greetVisitors() { 
+  
+  for (let visitor = 0; visitor < visitors.length; visitor++) {
+    let name = visitors[visitor];
+    console.log(`Hi ${myName}, my name is ${name}!`);
+  }
+ }
+ greetVisitors();
+
 // ========================
 const numbers = [2, 123, 13, 1, 6, 84, 23, 12, 45]; // DON'T TOUCH THIS LINE :)
 
@@ -20,6 +29,14 @@ const numbers = [2, 123, 13, 1, 6, 84, 23, 12, 45]; // DON'T TOUCH THIS LINE :)
  * If the item is not present, return a string "Item not found"
  */
 
+function getElementIndex(check) {
+  for (let index = 0; index < numbers.length; index++) {
+    const number = numbers[index];
+    if (number === check) {
+      return index;
+    }
+  } return 'Item not found';
+}
 // ========================
 
 /**
@@ -27,6 +44,13 @@ const numbers = [2, 123, 13, 1, 6, 84, 23, 12, 45]; // DON'T TOUCH THIS LINE :)
  * Create a function {total} that will return sum of all numbers
  * from {numbers} array
  */
+function total() {
+  let sum = 0
+  for (let index = 0; index < numbers.length; index++) {
+    const number = numbers[index];
+    sum += number;
+  } return sum;
+}
 
 // ========================
 
@@ -38,9 +62,26 @@ const numbersForIndexes = [6, 2, 16, 23, 84, 21, 9, 3]; // DON'T TOUCH THIS LINE
  * and returns a new array which includes each number + it's index
  */
 
+let newArray = [];
+function addIndex() {
+    for(let i = 0; i < numbersForIndexes.length; i++) {
+      newArray.push(numbersForIndexes[i] + i);
+    }
+    return newArray;
+  }
+
 const forLowestNumber = [5, 234, 96, 34, -34, 0, 23]; // DON'T TOUCH THIS LINE :)
 /**
  * Exercise 5
  *  create function {lowestNumber} which will return the lowest integer
  * from {forLowestNumber}
  */
+
+
+function lowestNumber() {
+  for (let i = 0; i < forLowestNumber.length; i++) {
+    if(forLowestNumber[i] < 0) {
+      return forLowestNumber[i];
+    }
+  }
+}
