3606db086869c4c30e78963d350da3088fe75204
Teddy Geo <reftea@gmail.com>
2020-07-21 09:50:34 +0100

    exercise 18 completed

--- module-1/18-adding-object-properties/index.js
+++ module-1/18-adding-object-properties/index.js
@@ -10,11 +10,15 @@ const user = {};
  * add property "firstName" to "user" and assign any name 
  */
 
+ user.firstName = 'Alex';
+
 /**
  * Exercise 2
  * 
  * add property "lastName" to "user" and assign any last name 
  */
+
+ user.lastName = 'Smith';
 
 /**
  * Exercise 3
@@ -22,3 +26,8 @@ const user = {};
  * 2 params, first - property name and second - property value
  * and set a new property for 'user' object
  */
+ function addANewProperty(key,value) {
+  return user[key] = value;
+}
+  addANewProperty('age', 23);
+  console.log(user);
