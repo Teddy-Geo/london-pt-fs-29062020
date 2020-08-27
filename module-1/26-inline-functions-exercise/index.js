a6ffe13a85a4847fd1316a964f57b211aa651de1
Teddy Geo <reftea@gmail.com>
2020-08-27 17:20:57 +0100

    26 completed

--- module-1/26-inline-functions-exercise/index.js
+++ module-1/26-inline-functions-exercise/index.js
@@ -10,14 +10,26 @@
  * use default value "[]", is you pass any other type than array
  * return a string "Please, pass correct type".
  */
-
+const reverseArray = function (array = []) {
+  if (Array.isArray(array)){
+    return array.reverse();
+  }
+  return "Please, pass correct type"
+}
 /**
  * Exercise 2
  *
  * create an inline function {nextOddNum} that takes number as a param
  * and return next odd number.
  */
-
+const nextOddNum = function (number) {
+  if (number % 2 !== 0) {
+    number += 2;
+  } else if (number % 2 === 0) {
+    number += 1;
+  }
+  return number;
+}
 /**
  * Exercise 3
  * 
@@ -26,13 +38,20 @@
  * Given two numbers, return true if the sum of both numbers is 
  * less than 100. Otherwise return false.
  */
-
+const isLessThan100 = function (num1, num2) {
+  if (num1 + num2 < 100) {
+    return true;
+  }
+  return false;
+}
 /**
  * Exercise 4
  * create an inline function {incrementBy5} which takes
  * a number as a param, add 5 and return the result.
  */
-
+const incrementBy5 = function (number) {
+return number + 5;
+}
 /**
  * Exercise 5
  * create a function {incrementAllBy5} which takes array
@@ -40,3 +59,10 @@
  * Then apply incrementor function to each element in your array and 
  * return the result
  */
+const incrementAllBy5 = function (array, incrementor) {
+  let newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    newArray.push(incrementor(array[i]));
+  }
+  return newArray;
+};
